dde8e1f04936bbf8445b7eec2bfc87b7d38221a2
unknown <nguyensongvo2652001@gmail.com>
2022-05-24 13:12:04 +0700

    Added global error handler

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 
+const errorController = require("./controllers/errorController");
+
 const app = express();
 
 app.use(express.json({ limit: process.env.MAX_REQUEST_BODY_SIZE }));
+
+app.use(errorController);
 
 module.exports = app;
