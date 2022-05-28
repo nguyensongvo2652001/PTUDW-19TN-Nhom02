b694aaf2309d5fa9e90417b7a750130fd242b9cd
Hai ha <thiensuha01@gmail.com>
2022-05-28 13:24:12 +0700

    Added order model

---
+++ src/models/orderModel.js
@@ -0,0 +1,134 @@
+const mongoose = require("mongoose");
+
+const checkoutSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "You must specify your name"],
+    maxlength: [50, "Your name is too long"],
+    trim: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: [true, "You must specify your phone number"],
+    maxlength: [20, "Your phone number is too long"],
+    trim: true,
+  },
+  province: {
+    type: String,
+    required: [true, "You must specify your province"],
+    maxlength: [100, "Your province name is too long"],
+    trim: true,
+  },
+  district: {
+    type: String,
+    required: [true, "You must specify your district"],
+    maxlength: [100, "Your district name is too long"],
+    trim: true,
+  },
+  ward: {
+    type: String,
+    required: [true, "You must specify your ward"],
+    maxlength: [100, "Your ward name is too long"],
+    trim: true,
+  },
+  address: {
+    type: String,
+    required: [true, "You must specify your address"],
+    maxlength: [300, "Your address is too long"],
+    trim: true,
+  },
+  order: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Order",
+    required: [true, "Checkout must belong to an order"],
+  },
+});
+
+const orderItemSchema = new mongoose.Schema({
+  order: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Order",
+    required: [true, "Order item must belong to an order"],
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: [true, "Order item must be one of the product"],
+  },
+  count: {
+    type: Number,
+    validate: {
+      validator: function (value) {
+        return value > 0;
+      },
+      message: "Number of products bought must be larger than 0",
+    },
+  },
+});
+
+/*QUERY MIDDLEWARE */
+//Populate product field
+orderSchema.pre(/^find/, async function (next) {
+  this.populate("product", "name price seller thumbnail dateAdded");
+  next();
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    dateOrdered: {
+      type: String,
+      default: Date.now,
+    },
+    buyer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Order must belong to an user"],
+    },
+    transportPrice: {
+      type: Number,
+      default: 0,
+    },
+    surcharge: {
+      type: Number,
+      default: 0,
+    },
+    totalPrice: {
+      type: Number,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+/*DOCUMENT MIDDLEWARE */
+//Calculate total price before save
+orderSchema.pre("save", async function (next) {
+  const orderItems = await OrderItem.find({ orderId: this._id });
+  const productsPrice = orderItems.reduce(
+    (acc, orderItem) => acc + orderItem.product.price * orderItem.count,
+    0
+  );
+  this.totalPrice = this.transportPrice + this.surcharge + productsPrice;
+  next();
+});
+/*************************************/
+
+orderSchema.virtual("checkout", {
+  ref: "Checkout",
+  localField: "_id",
+  foreignField: "order",
+});
+
+orderSchema.virtual("orderItems", {
+  ref: "OrderItem",
+  localField: "_id",
+  foreignField: "order",
+});
+
+const Order = mongoose.model("Order", orderSchema);
+const OrderItem = mongoose.model("OrderItem", orderItemSchema);
+const Checkout = mongoose.model("Checkout", checkoutSchema);
+
+module.exports = { Order, OrderItem, Checkout };
