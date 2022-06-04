ddc5853edbccb7c125d525976392fdb4770f6086
unknown <nguyensongvo2652001@gmail.com>
2022-06-04 21:26:54 +0700

    Added edit profile route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,10 @@
+const multer = require("multer");
+const sharp = require("sharp");
+
+const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const { filterObject } = require("../utils/helpers");
 
 const setCurrentUserId = (req, res, next) => {
   //Use this middleware for route with "/me/..."
@@ -6,7 +12,56 @@ const setCurrentUserId = (req, res, next) => {
   req.params.sellerId = req.user._id; //This is useful when we want to get all the products from the current user
   next();
 };
+const storage = multer.memoryStorage();
+const fileFilter = function (req, file, cb) {
+  const fileType = file.mimetype.split("/")[0];
+  const { fieldname } = file;
+  if (fileType !== "image")
+    return cb(
+      new AppError(
+        `${fieldname} must be an image. Please try another file`,
+        400
+      ),
+      false
+    );
+  cb(null, true);
+};
+const upload = multer({ storage, fileFilter });
 
-const editUserProfile = catchAsync(async (req, res, next) => {});
+const uploadAvatar = upload.single("avatar");
 
-module.exports = { setCurrentUserId };
+const resizeAndStoreAvatar = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+  const avatar = req.file.buffer;
+  if (!avatar) return next();
+  req.body.avatar = `user_${req.user.id}_avatar.jpg`;
+  await sharp(avatar)
+    .resize(500, 500)
+    .jpeg({ quality: 80 })
+    .toFile(`public/images/users/avatars/${req.body.avatar}`);
+
+  next();
+});
+
+const updateMe = catchAsync(async (req, res, next) => {
+  req.body = filterObject(req.body, {
+    whiteList: ["username", "storeName", "phoneNumber", "avatar"],
+  });
+
+  const user = await User.findByIdAndUpdate(req.user.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: { user },
+  });
+});
+
+module.exports = {
+  setCurrentUserId,
+  uploadAvatar,
+  resizeAndStoreAvatar,
+  updateMe,
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,21 @@
 const express = require("express");
 
 const authController = require("../controllers/authController");
+const userController = require("../controllers/userController");
 const productRouter = require("./productRouter");
 
 const router = express.Router();
 
 router.post("/login", authController.login);
 router.post("/signUp", authController.signUp);
+
+router.patch(
+  "/me",
+  authController.protect,
+  userController.uploadAvatar,
+  userController.resizeAndStoreAvatar,
+  userController.updateMe
+);
 
 router.use("/:sellerId/products", productRouter);
 router.use("/me/products", productRouter);
