525e33afd65d119a297bb9a6b5a085839d5ade38
Hai ha <thiensuha01@gmail.com>
2022-07-04 08:34:58 +0700

    Fix profile edit

--- src/public/javascript/profilePage.js
+++ src/public/javascript/profilePage.js
@@ -1,16 +1,12 @@
-const profilePage = document.querySelector(".profilePage");
-const editProfileForm = profilePage.querySelector(".popupProfileForm");
-const editProfileButton = profilePage.querySelector(".editProfileButton");
-const saveEditProfileButton = profilePage.querySelector(
-  ".saveEditProfileButton"
-);
-const cancelEditProfileButton = profilePage.querySelector(
+const profilePage = document.querySelector(".main-display");
+const editProfileForm = document.querySelector(".popupProfileForm");
+const editProfileButton = document.querySelector(".editProfileButton");
+const saveEditProfileButton = document.querySelector(".saveEditProfileButton");
+const cancelEditProfileButton = document.querySelector(
   ".cancelEditProfileButton"
 );
-const profileMainBoard =  profilePage.querySelector(
-  ".profileMainBoard"
-);
-
+const profileMainBoard = document.querySelector(".main-display");
+console.log(editProfileForm);
 const nameInput = editProfileForm.querySelector("#Username");
 const descriptionInput = editProfileForm.querySelector("#description");
 const phoneNumberInput = editProfileForm.querySelector("#PhoneNumber");
@@ -18,9 +14,8 @@ const shopNameInput = editProfileForm.querySelector("#shopName");
 const avatarInput = editProfileForm.querySelector("#avatar");
 
 editProfileButton.addEventListener("click", () => {
-  document.getElementsByClassName("profileMainBanner")[0].style.height = "140%";
   showElement(editProfileForm);
-  hideElement(profileMainBoard)
+  hideElement(profileMainBoard);
 });
 
 saveEditProfileButton.addEventListener("click", () => {
@@ -29,8 +24,7 @@ saveEditProfileButton.addEventListener("click", () => {
 
 cancelEditProfileButton.addEventListener("click", () => {
   hideElement(editProfileForm);
-  document.getElementsByClassName("profileMainBanner")[0].style.height = "";
-  showElement(profileMainBoard)
+  showElement(profileMainBoard);
 });
 
 const updateProfile = async () => {
@@ -41,8 +35,6 @@ const updateProfile = async () => {
     data["description"] = descriptionInput.value;
     data["phoneNumber"] = phoneNumberInput.value;
     data["storeName"] = shopNameInput.value;
-
-
 
     const formData = new FormData();
     Object.keys(data).forEach((key) => formData.append(key, data[key]));
@@ -56,8 +48,6 @@ const updateProfile = async () => {
     showElement(profileMainBoard);
     redirect("/api/v1/users/me");
   } catch {
-    console.log("error")
+    console.log("error");
   }
 };
-
-console.log("???");
